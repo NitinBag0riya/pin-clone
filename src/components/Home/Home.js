d1ecbc068d6ac6ed12cf46f51279d1af670583f8
Nitin Bagoriya <nitin.bagoriya@hippocabs.com>
2020-07-17 02:52:03 +0530

    addedLazyLoadingAPIfetchForMorePhotos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../layout/Header'
 import { Container, Col, Row } from 'react-bootstrap'
 import { Posts } from '../Posts/Posts'
+import { getPhotos } from '../../apis/FetchFromUnsplash'
 
 export function Home(){
+    const [photos, setPhotos ] = useState([])
+
+    useEffect( () => {
+        getPhotos().then(response => response.json()).then(images => setPhotos(images)).catch(e => console.log(e))
+    }, [])
+
+    const loadMorePhotos = (e) => {
+        const element = e.target;
+        if( element.scrollHeight - element.scrollTop === element.clientHeight){
+            getPhotos().then(response => response.json()).then(images => setPhotos(photos.concat(images))).catch(e => console.log(e))
+        }
+    }
+    
     return(
         <>
         <Header />
-        <Container>
-        <Row>
-            <Posts />
-        </Row>
+        <Container onScroll={e => loadMorePhotos(e)} style={{height:'100vH', backgroundColor:'coral',overflow : 'scroll'}}>
+            <Posts data={photos} />
         </Container>
         </>
     )
