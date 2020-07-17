1ab52478162b32314c666a61f951512ce4761c89
Nitin Bagoriya <nitin.bagoriya@hippocabs.com>
2020-07-17 08:05:54 +0530

    Done Navbar with keyword search and deboucing

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,35 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Form, Col } from 'react-bootstrap';
+import { getPhotosUsingKeyword } from '../apis/FetchFromUnsplash';
+import { useDispatch } from 'react-redux';
+import { USER_SELECTION_PHOTOS } from '../components/State/types';
+import { debounce } from 'nitin-js'
 
-function Header(){
+function Header(props){
+    const dispatch  = useDispatch()
+    const fetchPhotos = (keyword) => {
+        getPhotosUsingKeyword(keyword).then(response => response.json()).then(images => {
+            dispatch({
+                type : USER_SELECTION_PHOTOS,
+                payload : images.results
+              })
+        }).catch(e => console.log(e))
+    }
+
+    const searchUsingQuery = (query) => {
+        if(query.length > 3){
+            getPhotosUsingKeyword(query).then(response => response.json()).then(images => {
+                dispatch({
+                    type : USER_SELECTION_PHOTOS,
+                    payload : images.results
+                  })
+            }).catch(e => console.log(e))
+        }else{
+            alert('Please enter atleast 3 keyword')
+        }
+    }
+    const debouceUserSearch = debounce(searchUsingQuery, 1000)
+
     return(
         <>
         <Navbar bg="dark" className="navbar-sticky-" expand="lg" sticky="top">
@@ -10,15 +38,16 @@ function Header(){
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about-us">Latest</Nav.Link>
-                <Nav.Link href="#">Saved Pins</Nav.Link>
-                {/* <Form inline>
+                <Nav.Link onClick={ () => fetchPhotos('latest') }>Latest</Nav.Link>
+                <Nav.Link onClick={ () => fetchPhotos('covid') }>Covid-19</Nav.Link>
+                <Nav.Link onClick={ () => props.toggleSavedPins()}>Saved Pins</Nav.Link>
+                <Form inline>
                     <Form.Row>
                         <Col xs={7} md={12}>
-                        <Form.Control placeholder="City" />
+                        <Form.Control onChange={e => debouceUserSearch(e.target.value)} placeholder="City, Animal etc" />
                         </Col>
                     </Form.Row>
-                </Form> */}
+                </Form>
                 </Nav>                                    
             </Navbar.Collapse>
         </Navbar>
