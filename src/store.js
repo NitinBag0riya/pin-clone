da015e85be854fdb74bd97cfddb59ed100cdcb09
Nitin Bagoriya <nitin.bagoriya@hippocabs.com>
2020-07-21 23:57:14 +0530

    added state for savedPins

--- src/store.js
+++ src/store.js
@@ -2,8 +2,21 @@ const { createStore } = require("redux");
 const { reducer } = require("./components/State/Reducers");
 
 const initialState = {
-    pins : [],
-    photos : []
+    pins : [
+        // {
+        //     alt_description: "BharatMataKiJai",
+        //     heading: "BharatMataKiJai",
+        //     id: 406,
+        //     urls:{
+        //         full: "https://images.unsplash.com/photo-1595170296052-0ca2a380bed4",
+        //         raw: "https://images.unsplash.com/photo-1595170296052-0ca2a380bed4",
+        //         regular: "https://images.unsplash.com/photo-1595170296052-0ca2a380bed4",
+        //         small: "https://images.unsplash.com/photo-1595170296052-0ca2a380bed4",
+        //         thumb: "https://images.unsplash.com/photo-1595170296052-0ca2a380bed4"
+        //     }
+        // }
+    ],
+    savedPins : []
 }
 
 export const store = createStore(reducer,initialState)
