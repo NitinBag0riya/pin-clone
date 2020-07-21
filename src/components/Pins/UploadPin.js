4cc10c507a570e838c8f77de76a5e163edbbda6d
Nitin Bagoriya <nitin.bagoriya@hippocabs.com>
2020-07-21 23:52:40 +0530

    Upload Modal for custom pin upload

---
+++ src/components/Pins/UploadPin.js
@@ -0,0 +1,77 @@
+import React, { useRef } from 'react'
+import { Modal, Button, InputGroup, Form, Row, Image } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { USER_UPLOAD_PIN } from '../State/types';
+
+export function UploadPin(props) {
+    let titleRef = useRef('')
+    let imagePath = useRef('')
+
+    const dispatch  = useDispatch()
+
+
+    const uploadPinToState = () => {
+
+        const formattedPinObject = {
+            id: Math.floor(Math.random() * 1000), 
+            alt_description: titleRef.current.value,
+            heading : titleRef.current.value,
+            urls: {
+                full: imagePath.current.value,
+                raw: imagePath.current.value,
+                regular: imagePath.current.value,
+                small: imagePath.current.value,
+                thumb: imagePath.current.value,
+            }
+        }
+        
+        alert('clicked')
+        dispatch({
+          type : USER_UPLOAD_PIN,
+          payload : formattedPinObject
+        })
+
+        props.onHide()
+    }
+
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Upload Pin
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          
+        <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>Pin Title</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control type="text" placeholder=" Title..." ref={titleRef} />
+        </InputGroup>
+
+        <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>Image</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control type="text" placeholder=" Enter Image Url..." ref={imagePath} />
+        </InputGroup>
+
+        {/* preview of image */}
+        <Row>
+            <Image src={imagePath || 'holder.js/171x180' } thumbnail />
+        </Row>
+          
+        </Modal.Body>
+        <Modal.Footer>
+            <Button onClick={ () => uploadPinToState() }>Upload</Button>
+            <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
